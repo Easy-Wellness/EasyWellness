69136e303006fd831b4fc6f0278c993aef9a2f2d
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-06-14 10:41:27 +0700

    🚧 get geographic location from Place ID  in autocomplete prediction

---
+++ lib/utils/geo_location_from_place_id.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:users/constants/misc.dart';
+import 'package:users/models/geo_location.dart';
+import 'package:users/models/google_maps_geocode_result.dart';
+
+Future<GeoLocation> geoLocationFromPlaceId(String placeId) async {
+  final geocodingAPI = Uri.https(
+    'maps.googleapis.com',
+    '/maps/api/geocode/json',
+    {
+      'key': google_maps_api_key,
+      'place_id': placeId,
+    },
+  );
+  final response = await http.get(geocodingAPI);
+  if (response.statusCode == 200) {
+    final parsed = GoogleMapsGeocodeResult.fromJson(
+        jsonDecode(response.body)['results'][0]);
+    final geoCoords = parsed.geometry.location;
+    return GeoLocation(
+      latitule: geoCoords.lat,
+      longitude: geoCoords.lng,
+      address: parsed.formattedAddress,
+    );
+  } else
+    throw HttpException(
+      'Failed to find the location associated with this placeID',
+      uri: geocodingAPI,
+    );
+}
