847e00e6c3be37f94ae89988a703f181f4b9507d
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-08-02 12:02:53 +0700

    💄 add user profile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:users/components/custom_bottom_nav_bar.dart';
 
@@ -19,6 +20,43 @@ class ProfileScreen extends StatelessWidget {
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.maxFinite,
+      child: TextButtonTheme(
+        data: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: Colors.black87,
+            alignment: Alignment.centerLeft,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.account_circle_outlined),
+              label: Text('Manage your account'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.favorite_border),
+              label: Text('Favorites'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.reviews_outlined),
+              label: Text('Reviews'),
+            ),
+            TextButton.icon(
+              onPressed: () => FirebaseAuth.instance.signOut(),
+              icon: Icon(Icons.logout_outlined),
+              label: Text('Sign out'),
+              style:
+                  TextButton.styleFrom(primary: Theme.of(context).errorColor),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
