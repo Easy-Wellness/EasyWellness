f5f5b6bc9da6ab735d9102f92c7c17c0a415f19e
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-08-25 18:13:35 +0700

    👔 printObject should accept null object as param

--- lib/utils/print_object.dart
+++ lib/utils/print_object.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-void printObject(Object object) {
+void printObject(Object? object) {
+  if (object == null) return debugPrint('The object to print is null');
   // Encode your object and then decode your object to Map variable
   Map jsonMapped = json.decode(json.encode(object));
 
