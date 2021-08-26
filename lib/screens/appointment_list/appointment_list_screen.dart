b4d7ebe6ee1acc6469bfc849a2a1416a143ce25f
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-08-26 16:33:29 +0700

    🚚 make a list view of appointments reusable

--- lib/screens/appointment_list/appointment_list_screen.dart
+++ lib/screens/appointment_list/appointment_list_screen.dart
@@ -2,9 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:users/models/appointment/db_appointment.model.dart';
-import 'package:users/widgets/booking_summary.dart';
+import 'package:users/screens/appointment_list/appt_list_view.dart';
 import 'package:users/widgets/custom_bottom_nav_bar.dart';
+import 'package:users/models/appointment/db_appointment.model.dart';
 
 final _apptsRef = FirebaseFirestore.instance
     .collectionGroup('appointments')
@@ -85,7 +85,7 @@ class _CanceledTabViewState extends State<CanceledTabView> {
             final apptList = snapshot.data?.docs ?? [];
             if (apptList.isEmpty)
               return Text('You have no canceled appointments');
-            return _ApptListView(
+            return ApptListView(
               apptList: apptList,
               primaryBtnBuilder: (_, index) => ElevatedButton(
                 child: const Text('Book again'),
@@ -125,7 +125,7 @@ class _PastTabViewState extends State<PastTabView> {
           if (snapshot.connectionState == ConnectionState.done) {
             final apptList = snapshot.data?.docs ?? [];
             if (apptList.isEmpty) return Text('No results found');
-            return _ApptListView(
+            return ApptListView(
               apptList: apptList,
               primaryBtnBuilder: (_, index) => ElevatedButton.icon(
                 icon: const Icon(Icons.reviews_outlined),
@@ -170,7 +170,7 @@ class _UpcomingTabViewState extends State<UpcomingTabView> {
             final apptList = snapshot.data?.docs ?? [];
             if (apptList.isEmpty)
               return Text('You have no upcoming appointments');
-            return _ApptListView(
+            return ApptListView(
               apptList: apptList,
               primaryBtnBuilder: (_, index) => ElevatedButton.icon(
                 icon: const Icon(Icons.chat_bubble_outline),
@@ -185,65 +185,6 @@ class _UpcomingTabViewState extends State<UpcomingTabView> {
           }
           return const CircularProgressIndicator.adaptive();
         },
-      ),
-    );
-  }
-}
-
-class _ApptListView extends StatelessWidget {
-  const _ApptListView({
-    Key? key,
-    required this.apptList,
-    required this.primaryBtnBuilder,
-    this.secondaryBtnBuilder,
-  }) : super(key: key);
-
-  final List<QueryDocumentSnapshot<DbAppointment>> apptList;
-  final ElevatedButton Function(BuildContext, int) primaryBtnBuilder;
-  final OutlinedButton Function(BuildContext, int)? secondaryBtnBuilder;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scrollbar(
-      child: ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: apptList.length,
-        itemBuilder: (ctx, idx) {
-          final appt = apptList[idx].data();
-          return Card(
-            key: ValueKey<String>(apptList[idx].id),
-            elevation: 2,
-            shape: ContinuousRectangleBorder(
-              side: BorderSide(color: Colors.grey.withOpacity(0.2)),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  BookingSummary(
-                    serviceName: appt.serviceName,
-                    placeName: appt.placeName,
-                    address: appt.address,
-                    dateTime: appt.effectiveAt.toDate(),
-                  ),
-                  const Divider(thickness: 1),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      if (secondaryBtnBuilder != null)
-                        secondaryBtnBuilder!(context, idx),
-                      const SizedBox(width: 8),
-                      primaryBtnBuilder(context, idx),
-                      const SizedBox(width: 8),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          );
-        },
-        separatorBuilder: (_, __) => const SizedBox(height: 8),
       ),
     );
   }
---
+++ lib/screens/appointment_list/appt_list_view.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:users/widgets/booking_summary.dart';
+
+import 'package:users/models/appointment/db_appointment.model.dart';
+
+class ApptListView extends StatelessWidget {
+  const ApptListView({
+    Key? key,
+    required this.apptList,
+    required this.primaryBtnBuilder,
+    this.secondaryBtnBuilder,
+  }) : super(key: key);
+
+  final List<QueryDocumentSnapshot<DbAppointment>> apptList;
+  final ElevatedButton Function(BuildContext, int) primaryBtnBuilder;
+  final OutlinedButton Function(BuildContext, int)? secondaryBtnBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: apptList.length,
+        itemBuilder: (ctx, idx) {
+          final appt = apptList[idx].data();
+          return Card(
+            key: ValueKey<String>(apptList[idx].id),
+            elevation: 2,
+            shape: ContinuousRectangleBorder(
+              side: BorderSide(color: Colors.grey.withOpacity(0.2)),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  BookingSummary(
+                    serviceName: appt.serviceName,
+                    placeName: appt.placeName,
+                    address: appt.address,
+                    dateTime: appt.effectiveAt.toDate(),
+                  ),
+                  const Divider(thickness: 1),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      if (secondaryBtnBuilder != null)
+                        secondaryBtnBuilder!(context, idx),
+                      const SizedBox(width: 8),
+                      primaryBtnBuilder(context, idx),
+                      const SizedBox(width: 8),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (_, __) => const SizedBox(height: 8),
+      ),
+    );
+  }
+}
