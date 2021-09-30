8cb4b47ee8bc98e044377dcf2507f821b9d73372
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-09-30 17:54:01 +0700

    🏷️ update type of service

--- lib/models/nearby_service/db_nearby_service.model.dart
+++ lib/models/nearby_service/db_nearby_service.model.dart
@@ -10,21 +10,45 @@ class DbNearbyService {
     required this.ratingsTotal,
     required this.specialty,
     required this.serviceName,
+    required this.priceTag,
     required this.placeName,
     required this.placeId,
     required this.address,
+    required this.description,
+    required this.duration,
     required this.geoPosition,
+    required this.minuteIncrements,
+    required this.minLeadHours,
+    required this.maxLeadDays,
   });
 
   final String specialty;
   final String address;
+  final String description;
   final double rating;
+  final int duration;
+
+  /// The minutes between two bookings. The value is always <=24 hours
+  /// (= 1440 mins).
+  @JsonKey(name: 'minute_increments')
+  final int? minuteIncrements;
+
+  /// The allowed minimum of hours in advance the appointment can be booked.
+  @JsonKey(name: 'min_lead_hours')
+  final int? minLeadHours;
+
+  /// The allowed maximum of days in advance the appointment can be booked.
+  @JsonKey(name: 'max_lead_days')
+  final int? maxLeadDays;
 
   @JsonKey(name: 'ratings_total')
   final int ratingsTotal;
 
   @JsonKey(name: 'service_name', defaultValue: '')
   final String serviceName;
+
+  @JsonKey(name: 'price_tag')
+  final PriceTag priceTag;
 
   @JsonKey(name: 'geo_position')
   final GeoPosition geoPosition;
@@ -40,4 +64,30 @@ class DbNearbyService {
       _$DbNearbyServiceFromJson(json);
 
   Map<String, dynamic> toJson() => _$DbNearbyServiceToJson(this);
+}
+
+@JsonSerializable(anyMap: true, explicitToJson: true)
+class PriceTag {
+  PriceTag({
+    required this.type,
+    this.value,
+  });
+
+  final PriceType type;
+  final int? value;
+
+  factory PriceTag.fromJson(Map<String, dynamic> json) =>
+      _$PriceTagFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PriceTagToJson(this);
+}
+
+enum PriceType {
+  fixed,
+  startingAt,
+  hourly,
+  free,
+  varies,
+  contactUs,
+  notSet,
 }
--- lib/models/nearby_service/db_nearby_service.model.g.dart
+++ lib/models/nearby_service/db_nearby_service.model.g.dart
@@ -12,11 +12,18 @@ DbNearbyService _$DbNearbyServiceFromJson(Map json) {
     ratingsTotal: json['ratings_total'] as int,
     specialty: json['specialty'] as String,
     serviceName: json['service_name'] as String? ?? '',
+    priceTag:
+        PriceTag.fromJson(Map<String, dynamic>.from(json['price_tag'] as Map)),
     placeName: json['place_name'] as String? ?? '',
     placeId: json['place_id'] as String? ?? '',
     address: json['address'] as String,
+    description: json['description'] as String,
+    duration: json['duration'] as int,
     geoPosition: GeoPosition.fromJson(
         Map<String, dynamic>.from(json['geo_position'] as Map)),
+    minuteIncrements: json['minute_increments'] as int?,
+    minLeadHours: json['min_lead_hours'] as int?,
+    maxLeadDays: json['max_lead_days'] as int?,
   );
 }
 
@@ -24,10 +31,64 @@ Map<String, dynamic> _$DbNearbyServiceToJson(DbNearbyService instance) =>
     <String, dynamic>{
       'specialty': instance.specialty,
       'address': instance.address,
+      'description': instance.description,
       'rating': instance.rating,
+      'duration': instance.duration,
+      'minute_increments': instance.minuteIncrements,
+      'min_lead_hours': instance.minLeadHours,
+      'max_lead_days': instance.maxLeadDays,
       'ratings_total': instance.ratingsTotal,
       'service_name': instance.serviceName,
+      'price_tag': instance.priceTag.toJson(),
       'geo_position': instance.geoPosition.toJson(),
       'place_id': instance.placeId,
       'place_name': instance.placeName,
     };
+
+PriceTag _$PriceTagFromJson(Map json) {
+  return PriceTag(
+    type: _$enumDecode(_$PriceTypeEnumMap, json['type']),
+    value: json['value'] as int?,
+  );
+}
+
+Map<String, dynamic> _$PriceTagToJson(PriceTag instance) => <String, dynamic>{
+      'type': _$PriceTypeEnumMap[instance.type],
+      'value': instance.value,
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$PriceTypeEnumMap = {
+  PriceType.fixed: 'fixed',
+  PriceType.startingAt: 'startingAt',
+  PriceType.hourly: 'hourly',
+  PriceType.free: 'free',
+  PriceType.varies: 'varies',
+  PriceType.contactUs: 'contactUs',
+  PriceType.notSet: 'notSet',
+};
