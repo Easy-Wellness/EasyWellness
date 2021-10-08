0531c36860cc361a6e9bfca9e642298e2d60b670
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-10-08 17:13:30 +0700

    ✨ render a list of chat rooms

--- lib/screens/chat_room_list/chat_room_list_screen.dart
+++ lib/screens/chat_room_list/chat_room_list_screen.dart
@@ -1,22 +1,108 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:users/models/chat_room/db_chat_room.model.dart';
+import 'package:users/screens/chat_room_list/chat_screen.dart';
 import 'package:users/widgets/custom_bottom_nav_bar.dart';
 
-class ChatRoomListScreen extends StatelessWidget {
-  static const String routeName = '/chat_list';
+class ChatRoomListScreen extends StatefulWidget {
+  static const String routeName = '/chat_room_list';
+
+  @override
+  State<ChatRoomListScreen> createState() => _ChatRoomListScreenState();
+}
+
+class _ChatRoomListScreenState extends State<ChatRoomListScreen> {
+  final Stream<QuerySnapshot<DbChatRoom>> queryStream = FirebaseFirestore
+      .instance
+      .collection('chat_rooms')
+      .where('customer_id', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+      .orderBy('last_message_at', descending: true)
+      .withConverter<DbChatRoom>(
+        fromFirestore: (snapshot, _) => DbChatRoom.fromJson(snapshot.data()!),
+        toFirestore: (room, _) => room.toJson(),
+      )
+      .snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Your chats')),
-      body: Body(),
+      body: SafeArea(
+        child: Center(
+          child: StreamBuilder<QuerySnapshot<DbChatRoom>>(
+            stream: queryStream,
+            builder: (_, snapshot) {
+              if (snapshot.hasError) return const Text('Something went wrong');
+
+              if (snapshot.connectionState == ConnectionState.waiting)
+                return const CircularProgressIndicator.adaptive();
+
+              final chatRoomList = snapshot.data?.docs ?? [];
+              if (chatRoomList.isEmpty) return Text('No chat rooms found.');
+
+              return ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: chatRoomList.length,
+                itemBuilder: (_, index) {
+                  final roomId = chatRoomList[index].id;
+                  final roomData = chatRoomList[index].data();
+                  return ListTile(
+                    key: ValueKey(roomId),
+                    leading: Icon(Icons.question_answer_outlined),
+                    title: Text(
+                      roomData.customerName,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    subtitle: Text(
+                      roomData.lastMessage,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    ),
+                    trailing:
+                        Text(_formatTimestamp(roomData.lastMessageAt.toDate())),
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => ChatScreen(
+                          chatRoomId: roomId,
+                          placeId: roomData.placeId,
+                          placeName: roomData.placeName,
+                          customerId: roomData.customerId,
+                          customerName: roomData.customerName,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (_, __) => const Divider(height: 8),
+              );
+            },
+          ),
+        ),
+      ),
       bottomNavigationBar: CustomBottomNavBar(),
     );
   }
 }
 
-class Body extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+/// Show Absolute or Relative Timestamps.
+String _formatTimestamp(DateTime ts) {
+  final duration = DateTime.now().difference(ts).abs();
+  final dayOfWeek = DateFormat.yMMMMEEEEd().format(ts).substring(0, 3);
+  final month = DateFormat.yMMMMd().format(ts).substring(0, 3);
+  final nDays = duration.inDays;
+  final nHours = duration.inHours;
+  final nMinutes = duration.inMinutes;
+  final nSeconds = duration.inSeconds;
+  if (nHours < 24) {
+    if (nSeconds <= 60) return 'now';
+    if (nMinutes < 60) return '$nMinutes minutes';
+    return '$nHours hour${nHours > 1 ? 's' : ''}';
   }
+  if (nDays < 7) return dayOfWeek;
+  if (ts.year == DateTime.now().year) return '$month ${ts.day}';
+  return '$month ${ts.day}, ${ts.year.toString().substring(2)}';
 }
