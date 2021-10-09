593caada8a09984e866734b306b1e3446ea42d62
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-10-09 22:59:34 +0700

    💄 redesign the list of nearby services

--- lib/screens/explore/search_services_screen.dart
+++ lib/screens/explore/search_services_screen.dart
@@ -1,13 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:recase/recase.dart';
 import 'package:users/models/nearby_service/db_nearby_service.model.dart';
 import 'package:users/screens/service_detail/service_detail_screen.dart';
 import 'package:users/widgets/pick_location_screen.dart';
 
-class SearchServicesScreen extends StatelessWidget {
+class SearchServicesScreen extends StatefulWidget {
   SearchServicesScreen({
     Key? key,
     required this.center,
@@ -18,14 +20,28 @@ class SearchServicesScreen extends StatelessWidget {
   final String specialty;
 
   @override
+  State<SearchServicesScreen> createState() => _SearchServicesScreenState();
+}
+
+class _SearchServicesScreenState extends State<SearchServicesScreen> {
+  Stream<DocSnapshotList>? serviceListStream;
+
+  @override
+  void initState() {
+    serviceListStream =
+        _servicesAroundLocationStream(widget.center, widget.specialty);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Nearby Services')),
+      appBar: AppBar(title: Text('Services Around You')),
       body: SafeArea(
         child: Center(
           child: Material(
             child: StreamBuilder<DocSnapshotList>(
-              stream: _servicesAroundLocationStream(center, specialty),
+              stream: serviceListStream!,
               builder: (context, snapshot) {
                 if (snapshot.hasError)
                   return const Text('Something went wrong');
@@ -35,73 +51,12 @@ class SearchServicesScreen extends StatelessWidget {
 
                 final nearbyServices = snapshot.data ?? [];
                 if (nearbyServices.isEmpty)
-                  return const Text('No nearby service is found');
+                  return const Text('No nearby services are found');
                 return Scrollbar(
-                  child: ListView.separated(
-                    padding: const EdgeInsets.only(top: 8),
-                    itemCount: nearbyServices.length,
-                    separatorBuilder: (_, __) => Divider(thickness: 1),
-                    itemBuilder: (context, index) {
-                      final data = nearbyServices[index].data()!;
-                      final service = DbNearbyService.fromJson(data);
-                      final distance = Geolocator.distanceBetween(
-                            center.latitule,
-                            center.longitude,
-                            service.geoPosition.geopoint.latitude,
-                            service.geoPosition.geopoint.longitude,
-                          ) /
-                          1000;
-                      return InkWell(
-                        onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => ServiceDetailScreen(
-                              serviceId: nearbyServices[index].id,
-                              placeId: service.placeId,
-                            ),
-                          ),
-                        ),
-                        child: ListTile(
-                          leading: Column(
-                            children: [
-                              RatingBarIndicator(
-                                rating: service.rating,
-                                itemCount: 5,
-                                itemSize: 10,
-                                itemBuilder: (_, index) => Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Text('${service.rating}'),
-                            ],
-                          ),
-                          title: Text(service.serviceName),
-                          subtitle: Text(
-                            '${service.address} (${service.placeName})',
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 3,
-                          ),
-                          trailing: RichText(
-                            text: TextSpan(
-                              style: Theme.of(context).textTheme.bodyText2,
-                              children: [
-                                TextSpan(
-                                    text: '${distance.toStringAsFixed(2)} km'),
-                                WidgetSpan(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 2, bottom: 2),
-                                    child: Icon(Icons.near_me, size: 14),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
+                  child: ServiceListView(
+                    serviceList: nearbyServices,
+                    latitule: widget.center.latitule,
+                    longitude: widget.center.longitude,
                   ),
                 );
               },
@@ -111,6 +66,186 @@ class SearchServicesScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class ServiceListView extends StatelessWidget {
+  const ServiceListView({
+    Key? key,
+    required this.serviceList,
+    required this.latitule,
+    required this.longitude,
+  }) : super(key: key);
+
+  final List<DocumentSnapshot<Map<String, dynamic>>> serviceList;
+  final double latitule;
+  final double longitude;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: serviceList.length,
+      itemBuilder: (_, index) {
+        final serviceData =
+            DbNearbyService.fromJson(serviceList[index].data()!);
+        final priceTag = serviceData.priceTag;
+        String valueText = '';
+        String typeText = describeEnum(priceTag.type).titleCase;
+        switch (priceTag.type) {
+          case PriceType.fixed:
+          case PriceType.startingAt:
+          case PriceType.hourly:
+            valueText = '\$${priceTag.value}';
+            typeText = ' ($typeText)';
+            break;
+          default:
+        }
+        final distance = Geolocator.distanceBetween(
+              latitule,
+              longitude,
+              serviceData.geoPosition.geopoint.latitude,
+              serviceData.geoPosition.geopoint.longitude,
+            ) /
+            1000;
+        return Card(
+          key: ValueKey(serviceList[index].id),
+          elevation: 2,
+          shape: ContinuousRectangleBorder(
+            side: BorderSide(color: Colors.grey.withOpacity(0.2)),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: InkWell(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ServiceDetailScreen(
+                  serviceId: serviceList[index].id,
+                  placeId: serviceData.placeId,
+                ),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: Text(
+                    serviceData.serviceName,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  subtitle: Row(
+                    children: [
+                      RatingBarIndicator(
+                        rating: serviceData.rating,
+                        itemCount: 5,
+                        itemSize: 10,
+                        itemBuilder: (_, __) => const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text('${serviceData.rating}'),
+                      const SizedBox(width: 16),
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(color: Theme.of(context).hintColor),
+                          children: [
+                            WidgetSpan(
+                              alignment: PlaceholderAlignment.middle,
+                              child: Icon(
+                                Icons.near_me,
+                                size: 14,
+                                color: Theme.of(context).hintColor,
+                              ),
+                            ),
+                            TextSpan(text: '${distance.toStringAsFixed(2)} km'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.bodyText2,
+                      children: [
+                        const WidgetSpan(
+                          alignment: PlaceholderAlignment.middle,
+                          child: Icon(Icons.timer_outlined),
+                        ),
+                        TextSpan(text: _formatDuration(serviceData.duration)),
+                      ],
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: Theme.of(context).textTheme.bodyText2,
+                    children: [
+                      const WidgetSpan(
+                        alignment: PlaceholderAlignment.middle,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 12, right: 4),
+                          child: Icon(Icons.attach_money_outlined),
+                        ),
+                      ),
+                      TextSpan(text: valueText + typeText),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8),
+                RichText(
+                  text: TextSpan(
+                    style: Theme.of(context).textTheme.bodyText2,
+                    children: [
+                      const WidgetSpan(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 12, right: 4),
+                          child: Icon(Icons.place),
+                        ),
+                      ),
+                      TextSpan(text: 'At ${serviceData.placeName}'),
+                    ],
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+                const SizedBox(height: 8),
+                RichText(
+                  text: TextSpan(
+                    style: Theme.of(context).textTheme.bodyText2,
+                    children: [
+                      const WidgetSpan(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 12, right: 4),
+                          child: Icon(Icons.map_outlined),
+                        ),
+                      ),
+                      TextSpan(text: serviceData.address),
+                    ],
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+                const SizedBox(height: 8),
+              ],
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (_, __) => const SizedBox(height: 8),
+    );
+  }
+}
+
+String _formatDuration(int minutes) {
+  final duration = Duration(minutes: minutes);
+  final nHours = duration.inHours;
+  final hoursText = nHours >= 1 ? '${nHours}h ' : '';
+  final remainingMins = duration.inMinutes.remainder(60);
+  final minutesText = remainingMins == 0 ? '' : '$remainingMins mins';
+  return hoursText + minutesText;
 }
 
 typedef DocSnapshotList = List<DocumentSnapshot<Map<String, dynamic>>>;
