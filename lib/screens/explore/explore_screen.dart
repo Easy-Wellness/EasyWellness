fe7e2a2400598f8efdee6128cac9ff3fd4415ac4
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-10-25 21:50:15 +0700

    💄 add beautiful illustration to the explore screen

--- lib/screens/explore/explore_screen.dart
+++ lib/screens/explore/explore_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:users/widgets/custom_bottom_nav_bar.dart';
 
 import 'search_services_form.dart';
@@ -22,9 +23,19 @@ class ExploreScreen extends StatelessWidget {
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
       padding: const EdgeInsets.all(16),
-      child: SearchServicesForm(),
+      child: Column(
+        children: [
+          SearchServicesForm(),
+          const SizedBox(height: 40),
+          SvgPicture.asset(
+            "assets/icons/explore.svg",
+            height: size.height * 0.3,
+          ),
+        ],
+      ),
     );
   }
 }
